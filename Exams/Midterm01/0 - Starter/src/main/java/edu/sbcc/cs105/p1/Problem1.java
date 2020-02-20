f937ee97062bb5d3625d022a38fb4fb3896e1079
isaacchaidez <42746246+isaacchaidez@users.noreply.github.com>
2020-02-20 15:28:00 -0800

    Update Problem1.java

--- Exams/Midterm01/0 - Starter/src/main/java/edu/sbcc/cs105/p1/Problem1.java
+++ Exams/Midterm01/0 - Starter/src/main/java/edu/sbcc/cs105/p1/Problem1.java
@@ -8,7 +8,12 @@ import edu.sbcc.cs105.IProblem;
 public class Problem1 implements IProblem {
 
     public void run(){
+      RetirementCalculator calc = new RetirementCalculator(59, 30, 90600);
 
+        System.out.printf("The employee will receive %,4.2f / monthy or %,4.2f annually. This is %.2f%% of their employment income", 
+        calc.getMontlyRetirementBenefit(), 
+        calc.getAnnualRetirementBenefit(),
+        calc.getRetirementPercentage());
         
     }
 }
